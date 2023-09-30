b1cbb99bb7e2723c71de7ecad7bcaa2886adf019
oyekamal <autocratickamal1@gmail.com>
2023-09-30 09:57:03 +0500

    CRUD is working

--- apps/device/routes.py
+++ apps/device/routes.py
@@ -6,33 +6,39 @@ from apps.device.forms import DeviceForm
 from apps.device.models import Device
 from apps.device import blueprint
 
+
 @blueprint.route('/devices', methods=['GET', 'POST'])
 def devices():
     devices = Device.query.all()
     return render_template('devices/index.html', devices=devices)
 
+
 @blueprint.route('/devices/add', methods=['GET', 'POST'])
 def add_device():
-    print('add_device')
     form = DeviceForm()
     if form.validate_on_submit():
-        print("form.device_ip.data")
         try:
-            device = Device(
-                device_ip=form.device_ip.data,
-                device_name=form.device_name.data
-            )
-            db.session.add(device)
-            db.session.commit()
-            print(device)
-            flash('Device added successfully', 'success')
-            return redirect(url_for('device_blueprint.devices'))
+            # Check if the device_ip or device_name already exist in the database
+            existing_device = Device.query.filter(
+                (Device.device_ip == form.device_ip.data) |
+                (Device.device_name == form.device_name.data)
+            ).first()
+
+            if existing_device:
+                flash('Device with the same IP or Name already exists', 'error')
+            else:
+                device = Device(
+                    device_ip=form.device_ip.data,
+                    device_name=form.device_name.data
+                )
+                db.session.add(device)
+                db.session.commit()
+                flash('Device added successfully', 'success')
+                return redirect(url_for('device_blueprint.devices'))
         except Exception as e:
             db.session.rollback()  # Rollback the session in case of an error
             flash('An error occurred while adding the device', 'error')
             print(str(e))  # Print the error for debugging
-    else:
-        print('nhi howw')
     return render_template('devices/add.html', form=form)
 
 
@@ -47,6 +53,7 @@ def edit_device(id):
         flash('Device updated successfully', 'success')
         return redirect(url_for('device_blueprint.devices'))
     return render_template('devices/edit.html', form=form, device=device)
+
 
 @blueprint.route('/devices/delete/<int:id>', methods=['POST'])
 def delete_device(id):
